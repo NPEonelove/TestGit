8dcc1e83bea080e1070580852b61a89f19d486e7
NPEonelove <124193334+NPEonelove@users.noreply.github.com>
2025-09-08 15:13:19 +0400

    добавил докер и конфигурацию

---
+++ src/main/resources/db.changelog/versions/2025-07-08__create_profiles_table.sql
@@ -0,0 +1,9 @@
+-- create table profiles
+-- (
+--     profile_id uuid primary key default gen_random_uuid(),
+--     user_id    uuid unique                                        not null,
+--     username   varchar(32) check ( length(username) >= 5 ) unique not null,
+--     nickname   varchar(64)                                        not null,
+--     age        int check ( 14 <= age and age <= 150)              not null,
+--     photo_link varchar(512)
+-- )
